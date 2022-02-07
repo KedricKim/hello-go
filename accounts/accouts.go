814d06a57ef72374d7b0326194ab7a3a23a560bd
ked <kedric.me@gmail.com>
2022-02-07 11:48:06 +0900

    method practice 및 주석처리

--- accounts/accouts.go
+++ accounts/accouts.go
@@ -1,7 +1,11 @@
 package accounts
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
+// 지역변수
 var errNoMoney = errors.New("u dont have enough money")
 
 // Account struct
@@ -10,23 +14,41 @@ type Account struct {
 	balance int
 }
 
+// * 와 &의 사용
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
 
+//Struct method
 func (a *Account) Deposit(balance int) {
 	a.balance += balance
 }
 
+// *가 없으면 복사, 있으면 기존 메모리
 func (a Account) ShowDeposit() int {
 	return a.balance
 }
 
+// error exceprion
+// 여기서는 지역번수로 선언한 errNoMoney 를 return 하거나
+// 정상인 경우 return 값을 줘야 하기 때문에 nil 사용
 func (a *Account) WithDraw(balance int) error {
 	if a.balance < balance {
 		return errNoMoney
 	}
 	a.balance -= balance
 	return nil
+}
+
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), " has ", a.ShowDeposit(), " won")
 }
--- main.go
+++ main.go
@@ -12,12 +12,14 @@ func main() {
 	// basicPratice()
 
 	account := accounts.NewAccount("ked")
-	account.Deposit(1000)
-	err := account.WithDraw(9900)
+	account.Deposit(100)
+	err := account.WithDraw(20)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(account.ShowDeposit())
+	fmt.Println(account.ShowDeposit(), account.Owner())
+
+	fmt.Println(account)
 }
 
 func basicPratice() {
@@ -46,12 +48,14 @@ func multiple(a int, b int) int {
 	return a * b
 }
 
+// defer 은 func이 다 실행되고 나서 실행
 func lenAndString(name string) (int, string) {
 	defer fmt.Println("Hola!!")
 	fmt.Println(name)
 	return len(name), strings.ToUpper(name)
 }
 
+// for index, item
 func forLoop(numbers ...int) int {
 	total := 0
 	for _, number := range numbers {
@@ -67,6 +71,7 @@ func canIDrink(age int) string {
 	return "of cource drink it !"
 }
 
+// account 와 다르게 같은 패키지 내에서는 소문자
 type person struct {
 	name    string
 	age     int
